c1914637184eb952dc8d25bc660514bcfe60ee11
son1cman <kmson1cman@gmail.com>
2017-06-12 11:41:53 -0600

    DBHelper
    Added functionality to CRUD

--- app/src/main/java/com/factura/tomza/tomza/DatabaseHelper.java
+++ app/src/main/java/com/factura/tomza/tomza/DatabaseHelper.java
@@ -12,10 +12,13 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     //Constants for Database name, table name, and column names
-    public static final String DB_CLIENTES = "CLIENTESDB";
-    public static final String DB_FACTURATOMZA = "FACTURATOMZADB";
-    public static final String DB_FACTURACILZA = "FACTURACILZADB";
-    public static final String DB_GENERAL = "GENERALDB";
+    public static final String DB_TOMZA = "TOMZADB";
+
+    public static final String TABLE_CLIENTES = "CLIENTES";
+    public static final String TABLE_FACTURATOMZA = "FACTURATOMZA";
+    public static final String TABLE_FACTURACILZA = "FACTURACILZA";
+    public static final String TABLE_GENERAL = "GENERAL";
+
     public static final String CLIENTES_ID = "id";
     public static final String CLIENTES_FECHACREACION = "fechacreacion";
     public static final String CLIENTES_RUTA = "ruta";
@@ -44,17 +47,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String CLIENTES_COORDENADASLONG = "coordenadaslong";
     public static final String CLIENTES_COORDENADASLAT = "coordenadaslat";
 
-    public static final String FACTURATOMZA_ID = "";
-    public static final String FACTURATOMZA_FACNUM = "";
-    public static final String FACTURATOMZA_CODIGOCLIENTE = "";
-    public static final String FACTURATOMZA_Q20 = "";
-    public static final String FACTURATOMZA_Q25 = "";
-    public static final String FACTURATOMZA_Q35 = "";
-    public static final String FACTURATOMZA_Q45 = "";
-    public static final String FACTURATOMZA_Q100 = "";
-    public static final String FACTURATOMZA_QLTS = "";
-    public static final String FACTURATOMZA_QKGS = "";
-    public static final String FACTURATOMZA_FECHA = "";
+    public static final String FACTURATOMZA_ID = "id";
+    public static final String FACTURATOMZA_FACNUM = "facnum";
+    public static final String FACTURATOMZA_CODIGOCLIENTE = "codigocliente";
+    public static final String FACTURATOMZA_STATUS = "status";
+    public static final String FACTURATOMZA_Q20 = "q20";
+    public static final String FACTURATOMZA_Q25 = "q25";
+    public static final String FACTURATOMZA_Q35 = "q35";
+    public static final String FACTURATOMZA_Q45 = "q45";
+    public static final String FACTURATOMZA_Q100 = "q100";
+    public static final String FACTURATOMZA_QLTS = "qlts";
+    public static final String FACTURATOMZA_QKGS = "qkgs";
+    public static final String FACTURATOMZA_FECHA = "fecha";
 
     public static final String FACTURACILZA_ID = "";
     public static final String FACTURACILZA_FACNUM = "";
@@ -69,7 +73,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String GENERAL_ID = "";
     public static final String GENERAL_FECHA = "";
     public static final String GENERAL_LASTFACNUM = "";
-    public static final String GENERAL_PRECIO = "";
+    public static final String GENERAL_PRECIO20 = "";
+    public static final String GENERAL_PRECIO25 = "";
+    public static final String GENERAL_PRECIO35 = "";
+    public static final String GENERAL_PRECIO45 = "";
+    public static final String GENERAL_PRECIO100 = "";
+    public static final String GENERAL_PRECIOLTS = "";
+    public static final String GENERAL_PRECIOCIL20 = "";
+    public static final String GENERAL_PRECIOCIL25 = "";
+    public static final String GENERAL_PRECIOCIL35 = "";
+    public static final String GENERAL_PRECIOCIL45 = "";
+    public static final String GENERAL_PRECIOCIL100 = "";
     public static final String GENERAL_RUTA = "";
     public static final String GENERAL_CANAL = "";
     public static final String GENERAL_SUBCANAL = "";
@@ -86,13 +100,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     //Constructor
     public DatabaseHelper(Context context) {
-        super(context, DB_CLIENTES, null, DB_VERSION);
+        super(context, DB_TOMZA, null, DB_VERSION);
     }
 
     //creating the database
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "CREATE TABLE " + DB_CLIENTES
+        String sql = "CREATE TABLE " + TABLE_CLIENTES
                 + "(" + CLIENTES_ID +
                 " INTEGER PRIMARY KEY AUTOINCREMENT, " + CLIENTES_RUTA +
                 " VARCHAR, " + CLIENTES_CODIGO + " INTEGER, " +
@@ -100,21 +114,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                                  CLIENTES_CEDI +
                 " VARCHAR, " + CLIENTES_NOMBRE +
                 " VARCHAR, " + CLIENTES_RAZONSOCIAL +
-                " VARCHAR, "+ CLIENTES_TIPODEDOCUMENTO +
+                " VARCHAR, " + CLIENTES_TIPODEDOCUMENTO +
                 " VARCHAR, " + CLIENTES_DOCUMENTO +
                 " VARCHAR, " + CLIENTES_DIRECCION +
                 " VARCHAR, " + CLIENTES_TELEFONOFIJO +
-                " VARCHAR, " + CLIENTES_TELEFONOCELULAR+
-                " TINYINT, " +  CLIENTES_L +
+                " VARCHAR, " + CLIENTES_TELEFONOCELULAR +
+                " VARCHAR, " + CLIENTES_L +
                 " TINYINT, " + CLIENTES_K +
                 " TINYINT, " + CLIENTES_M +
                 " TINYINT, " + CLIENTES_J +
                 " TINYINT, " + CLIENTES_V +
                 " TINYINT, " + CLIENTES_S +
-                " VARCHAR, " + CLIENTES_FRECUENCIA +
-                " VARCHAR, " + CLIENTES_ORDEN +
+                " TINYINT, " + CLIENTES_FRECUENCIA +
+                " INTEGER, " + CLIENTES_ORDEN +
                 " INTEGER, " + CLIENTES_DESCUENTO +
-                " TINYINT, " + CLIENTES_CREDITO +
+                " DOUBLE, " + CLIENTES_CREDITO +
                 " TINYINT, " + CLIENTES_PUEDOFACTURAR +
                 " TINYINT, " + CLIENTES_FORMADEPAGO +
                 " INTEGER, " + CLIENTES_PLAZO +
@@ -122,12 +136,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 " VARCHAR, " + CLIENTES_COORDENADASLAT +" VARCHAR);";
         db.execSQL(sql);
 
-         sql = "CREATE TABLE " + DB_FACTURATOMZA
+         sql = "CREATE TABLE " + TABLE_FACTURATOMZA
                 + "(" + FACTURATOMZA_ID +
                 " INTEGER PRIMARY KEY AUTOINCREMENT, " + FACTURATOMZA_CODIGOCLIENTE +
                 " VARCHAR, " + FACTURATOMZA_FACNUM +
                 " INTEGER, "+ FACTURATOMZA_FECHA +
-                " TEXT, " + FACTURATOMZA_Q20 +
+                 " TEXT, "+ FACTURATOMZA_STATUS +
+                " INTEGER, " + FACTURATOMZA_Q20 +
                 " TINYINT, " + FACTURATOMZA_Q25 +
                 " TINYINT, " + FACTURATOMZA_Q35 +
                 " TINYINT, " + FACTURATOMZA_Q45 +
@@ -137,6 +152,29 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 " INTEGER);";
         db.execSQL(sql);
 
+
+        sql = "CREATE TABLE " + TABLE_GENERAL
+                + "(" + GENERAL_ID +
+                " INTEGER PRIMARY KEY AUTOINCREMENT, " + GENERAL_LASTFACNUM +
+                " VARCHAR, " + GENERAL_FECHA +
+                " TEXT, "+  GENERAL_PRECIO20 +
+                " INTEGER, " + GENERAL_PRECIO25 +
+                " INTEGER, " + GENERAL_PRECIO35 +
+                " INTEGER, " + GENERAL_PRECIO45 +
+                " INTEGER, " + GENERAL_PRECIO100 +
+                " INTEGER, " + GENERAL_PRECIOLTS +
+                " DOUBLE," + GENERAL_PRECIOCIL20 +
+                " INTEGER, " + GENERAL_PRECIOCIL20 +
+                " INTEGER, " + GENERAL_PRECIOCIL25 +
+                " INTEGER, " + GENERAL_PRECIOCIL35 +
+                " INTEGER, " + GENERAL_PRECIOCIL45 +
+                " INTEGER, " + GENERAL_PRECIOCIL100 +
+                " INTEGER, " + GENERAL_RUTA +
+                " INTEGER, " + GENERAL_SUBCANAL +
+                " VARCHAR(20)," + GENERAL_CANAL + " VARCHAR);";
+        db.execSQL(sql);
+
+
     }
 
     //upgrading the database
@@ -145,6 +183,102 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         String sql = "DROP TABLE IF EXISTS Persons";
         db.execSQL(sql);
         onCreate(db);
+    }
+
+
+    public boolean addFactura(Integer CODIGOCLIENTE, String FACNUM , String FECHA, int Q20,
+                              int Q25,int Q35,int Q45,int Q100,int QLTS,int QKGS) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(FACTURATOMZA_CODIGOCLIENTE, CODIGOCLIENTE);
+        contentValues.put(FACTURATOMZA_FACNUM, FACNUM);
+        contentValues.put(FACTURATOMZA_FECHA, FECHA);
+        contentValues.put(FACTURATOMZA_STATUS, 1);
+        contentValues.put(FACTURATOMZA_Q20, Q20);
+        contentValues.put(FACTURATOMZA_Q25, Q25);
+        contentValues.put(FACTURATOMZA_Q35, Q35);
+        contentValues.put(FACTURATOMZA_Q45, Q45);
+        contentValues.put(FACTURATOMZA_Q100, Q100);
+        contentValues.put(FACTURATOMZA_QLTS, QLTS);
+        contentValues.put(FACTURATOMZA_QKGS, QKGS);
+
+
+        db.insert(TABLE_FACTURATOMZA, null, contentValues);
+        db.close();
+        return true;
+    }
+
+    public boolean addGeneral(String LASTFACNUM,String FECHAG, String PRECIO20,int PRECIO25
+                             ,int PRECIO35,int PRECIO45,int PRECIO100, double PRECIOLTS, int PRECIOCIL20, int PRECIOCIL25, int PRECIOCIL35
+                             ,int PRECIOCIL45, int PRECIOCIL100, int RUTA, String SUBCANAL,String CANAL){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(GENERAL_LASTFACNUM, LASTFACNUM);
+        contentValues.put(GENERAL_FECHA, FECHAG);
+        contentValues.put(GENERAL_PRECIO20, PRECIO20);
+        contentValues.put(GENERAL_PRECIO25, PRECIO25);
+        contentValues.put(GENERAL_PRECIO35, PRECIO35);
+        contentValues.put(GENERAL_PRECIO45, PRECIO45);
+        contentValues.put(GENERAL_PRECIO100, PRECIO100);
+        contentValues.put(GENERAL_PRECIOLTS, PRECIOLTS);
+        contentValues.put(GENERAL_PRECIOCIL20, PRECIOCIL20);
+        contentValues.put(GENERAL_PRECIOCIL25, PRECIOCIL25);
+        contentValues.put(GENERAL_PRECIOCIL35, PRECIOCIL35);
+        contentValues.put(GENERAL_PRECIOCIL45, PRECIOCIL45);
+        contentValues.put(GENERAL_PRECIOCIL100, PRECIOCIL100);
+        contentValues.put(GENERAL_RUTA, RUTA);
+        contentValues.put(GENERAL_SUBCANAL, SUBCANAL);
+        contentValues.put(GENERAL_CANAL, CANAL);
+
+
+        db.insert(TABLE_GENERAL, null, contentValues);
+        db.close();
+
+
+        return true;
+    }
+
+    public boolean addCliente(int RUTA, int CODIGO, String FECHACREACION, String CEDI, String NOMBRE, String RAZONSOCIAL, String TIPOD,String DOC,
+                              String DIR,String TELF,String TELC, int L, int K, int M,int J,int V, int S,int FREQ, int ORD, double DESC,int CREDITO
+                              ,int PUEDOFACTURAR,int FORMADEPAGO, int PLAZO, String LAT, String LONG){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(CLIENTES_RUTA, RUTA);
+        contentValues.put(CLIENTES_CODIGO, CODIGO);
+        contentValues.put(CLIENTES_FECHACREACION, FECHACREACION);
+        contentValues.put(CLIENTES_CEDI, CEDI);
+        contentValues.put(CLIENTES_NOMBRE, NOMBRE);
+        contentValues.put(CLIENTES_RAZONSOCIAL, RAZONSOCIAL);
+        contentValues.put(CLIENTES_TIPODEDOCUMENTO, TIPOD);
+        contentValues.put(CLIENTES_DOCUMENTO, DOC);
+        contentValues.put(CLIENTES_DIRECCION, DIR);
+        contentValues.put(CLIENTES_TELEFONOFIJO, TELF);
+        contentValues.put(CLIENTES_TELEFONOCELULAR, TELC);
+        contentValues.put(CLIENTES_L,L);
+        contentValues.put(CLIENTES_K,K);
+        contentValues.put(CLIENTES_M,M);
+        contentValues.put(CLIENTES_J,J);
+        contentValues.put(CLIENTES_V,V);
+        contentValues.put(CLIENTES_S,S);
+        contentValues.put(CLIENTES_FRECUENCIA,FREQ);
+        contentValues.put(CLIENTES_ORDEN,ORD);
+        contentValues.put(CLIENTES_DESCUENTO,DESC);
+        contentValues.put(CLIENTES_CREDITO,CREDITO);
+        contentValues.put(CLIENTES_PUEDOFACTURAR,PUEDOFACTURAR);
+        contentValues.put(CLIENTES_FORMADEPAGO,FORMADEPAGO);
+        contentValues.put(CLIENTES_PLAZO,PLAZO);
+        contentValues.put(CLIENTES_COORDENADASLONG,LONG);
+        contentValues.put(CLIENTES_COORDENADASLAT,LAT);
+
+        db.insert(TABLE_CLIENTES, null, contentValues);
+        db.close();
+        return true;
+
     }
 
     /*
@@ -185,9 +319,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     /*
     * this method will give us all the name stored in sqlite
     * */
-    public Cursor getNames() {
+    public Cursor getFacturas() {
         SQLiteDatabase db = this.getReadableDatabase();
-        String sql = "SELECT * FROM " + TABLE_NAME + " ORDER BY " + COLUMN_ID + " ASC;";
+        String sql = "SELECT * FROM " + TABLE_FACTURATOMZA + " ORDER BY " + FACTURATOMZA_ID + " ASC;";
+        Cursor c = db.rawQuery(sql, null);
+        return c;
+    }
+    public Cursor getGeneral() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String sql = "SELECT * FROM " + TABLE_GENERAL + " ORDER BY " + GENERAL_ID + " ASC;";
         Cursor c = db.rawQuery(sql, null);
         return c;
     }
